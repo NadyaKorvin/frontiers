5e03edc3c6b4e891becf541e12ac45f5d419e77b
Aleksandr <tchistyakov1987@gmail.com>
2022-06-03 14:16:54 +0300

    add emailjs

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,12 +1,18 @@
-import React, { useState } from "react"
+import React, { useState, useRef } from "react"
 import { useForm } from "react-hook-form"
 import Button from "../button/Button.js"
 import Personal from "./PersonalData"
 import ModalSuccess from "./ModalSuccess"
 import ModalError from "./ModalError"
 import Preloader from "./Preloader"
+import emailjs from "emailjs-com";
+
+const SERVICE_ID = 'service_woillgx'
+const TEMPLATE_ID = 'template_is6o93c'
+const PUBLIC_KEY = 'voBy1eSygtB9fTOr2'
 
 export default function Form() {
+  const form = useRef();
   const [isPersonal, setIsPersonal] = useState(false)
   const [isActiveModalSuccess, setActiveModalSuccess] = useState(false)
   const [isActiveModalError, setActiveModalError] = useState(false)
@@ -19,20 +25,20 @@ export default function Form() {
     reset
   } = useForm({})
 
-  const onSubmit = (data) => {
+  const onSubmit = () => {
     setIsLoading(true)
-    // emailjs
-    // .sendForm(SERVICE_ID, TEMPLATE_ID, form.current, USER_ID)
-    // .then((res) => {
+    emailjs
+    .sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY)
+    .then((res) => {
     setActiveModalSuccess(true)
     setTimeout(setActiveModalSuccess, 5000)
     setIsLoading(false)
-    // });
-    // .catch((err) => {
-    //   setActiveModalError(true)
-    //   setTimeout(setActiveModalError, 5000);
-    //   setIsLoading(false);
-    // });
+    })
+    .catch((err) => {
+      setActiveModalError(true)
+      setTimeout(setActiveModalError, 5000);
+      setIsLoading(false);
+    });
     reset()
   }
 
@@ -43,9 +49,6 @@ export default function Form() {
     setIsPersonal(false)
   }
 
-  const openModal = () => {
-    setActiveModalSuccess(true)
-  }
   const clouseModal = () => {
     setActiveModalSuccess(false)
     setActiveModalError(false)
