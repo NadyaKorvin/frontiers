6f0b74dc22aa7eb8deab53af57a28cc29896aedf
NadyaKorvin <nadya.korvin@gmail.com>
2022-12-22 04:17:43 +0300

    fix 375 project card, only one opening

--- src/components/projects/projectCardsSmallMobail/ProjectCardsSmallMobail.js
+++ src/components/projects/projectCardsSmallMobail/ProjectCardsSmallMobail.js
@@ -4,11 +4,28 @@ import { DataProjects } from "../DataProjects"
 export function ProjectCardsSmallMobail({ handleValue }) {
   const [showDescription, setShowDescription] = useState(false)
   const [useKey, setUseKey] = useState("")
+  const [useProjectKey, setUseProjectKey] = useState("")
   const [openFullProjectDescription, setOpenFullProjectDescription] = useState(false)
 
   const findElementOnTap = (event, key) => {
     setUseKey(event.target.parentElement.parentElement.className)
     event.target.parentElement.parentElement.className === key ? setShowDescription(!showDescription) : setShowDescription(false)
+  }
+
+  const findProjectOnTap = (event, key) => {
+    event.target.parentElement.parentElement.parentElement.parentElement.className === key
+      ? setUseProjectKey(event.target.parentElement.parentElement.parentElement.parentElement.className)
+      : event.target.parentElement.parentElement.parentElement.className === key
+      ? setUseProjectKey(event.target.parentElement.parentElement.parentElement.className)
+      : event.target.parentElement.parentElement.className === key
+      ? setUseProjectKey(event.target.parentElement.parentElement.className)
+      : setUseProjectKey("")
+
+    event.target.parentElement.parentElement.parentElement.parentElement.className === key ||
+    event.target.parentElement.parentElement.parentElement.className === key ||
+    event.target.parentElement.parentElement.className === key
+      ? setOpenFullProjectDescription(!openFullProjectDescription)
+      : setOpenFullProjectDescription(false)
   }
 
   return (
@@ -17,30 +34,32 @@ export function ProjectCardsSmallMobail({ handleValue }) {
         {DataProjects.map((elem) =>
           handleValue <= elem.min ? (
             // Инвестиции ниже минимльных? РИСУЕМ СЕРУЮ КАРТОЧКУ
-            //1. Див для поиска нужной карточки. При клике на карточку ее ключ пректа записывается в стейт
+            //1. Общий блок
             <div className="small_mobail_project__card" key={elem.key + "small_mobail_project__card"}>
-              {/* 2.  Див с видимой и невидимой частью карточки */}
+              {/* 2.  Изменяемый Див с видимой и невидимой частью карточки */}
               <div
                 className={
-                  openFullProjectDescription
+                  openFullProjectDescription && useProjectKey === elem.key
                     ? "small_mobail_project_card__grey small_mobail_project_card__grey__open"
                     : "small_mobail_project_card__grey"
                 }
               >
                 {/* 3. Всегда видимая часть */}
-                <div className="small_mobail_project_card_grey__visible" onClick={() => setOpenFullProjectDescription(!openFullProjectDescription)}>
-                  <div className="small_mobail_project_card__grey_logo" style={{ backgroundImage: `url('${elem.grey_logo}')` }}></div>
-                  <div className="small_mobail_project_card__min_invest">
-                    <p className="small_mobail_project_card_min_invest__numbers">{Number(elem.min).toLocaleString("ru-RU") + " ₽"}</p>
-                    <p className="small_mobail_project_card_min_invest__description">Мин. размер инвестиций</p>
+                <div className={elem.key} onClick={(event) => findProjectOnTap(event, elem.key)}>
+                  <div className="small_mobail_project_card_grey__visible">
+                    <div className="small_mobail_project_card__grey_logo" style={{ backgroundImage: `url('${elem.grey_logo}')` }}></div>
+                    <div className="small_mobail_project_card__min_invest">
+                      <p className="small_mobail_project_card_min_invest__numbers">{Number(elem.min).toLocaleString("ru-RU") + " ₽"}</p>
+                      <p className="small_mobail_project_card_min_invest__description">Мин. размер инвестиций</p>
+                    </div>
+                    <div
+                      className={
+                        openFullProjectDescription && useProjectKey === elem.key
+                          ? "small_mobail_project_card__arrow_to_down_grey small_mobail_project_card__arrow_to_up_grey"
+                          : "small_mobail_project_card__arrow_to_down_grey"
+                      }
+                    ></div>
                   </div>
-                  <div
-                    className={
-                      openFullProjectDescription
-                        ? "small_mobail_project_card__arrow_to_down_grey small_mobail_project_card__arrow_to_up_grey"
-                        : "small_mobail_project_card__arrow_to_down_grey"
-                    }
-                  ></div>
                 </div>
                 {/* 4. Раскрывающийся блок */}
                 <div className="small_mobail_after_click__project_block">
@@ -118,47 +137,49 @@ export function ProjectCardsSmallMobail({ handleValue }) {
               {/* 2.  Див с видимой и невидимой частью карточки */}
               <div
                 className={
-                  openFullProjectDescription
+                  openFullProjectDescription && useProjectKey === elem.key
                     ? "small_mobail_project_card__color small_mobail_project_card__color__open"
                     : "small_mobail_project_card__color"
                 }
               >
                 {/* 3. Всегда видимая часть */}
-                <div
-                  className="small_mobail_project_card__color_visible"
-                  style={{ backgroundImage: `url('${elem.background}')` }}
-                  onClick={() => setOpenFullProjectDescription(!openFullProjectDescription)}
-                >
-                  <div className="small_mobail_project_card__logo" style={{ backgroundImage: `url('${elem.logo}')` }}></div>
-                  <div className="small_mobail_project_card__income">
-                    <p className="small_mobail_project_card_income_percent_numbers">
-                      {handleValue < elem.mid
-                        ? elem.income_min + "% годовых"
-                        : handleValue >= elem.mid && handleValue < elem.max
-                        ? elem.income_mid + "% годовых"
-                        : handleValue >= elem.max && handleValue < elem.ultra
-                        ? elem.income_max + "% годовых"
-                        : elem.income_ultra + "% годовых"}
-                    </p>
-                    <div className="small_mobail_project_card__income_money">
-                      <p className="small_mobail_project_card_income_money_numbers">
-                        {handleValue < elem.mid
-                          ? Number((handleValue * elem.income_min) / 100).toLocaleString("ru-RU") + " ₽ в год"
-                          : handleValue >= elem.mid && handleValue < elem.max
-                          ? Number((handleValue * elem.income_mid) / 100).toLocaleString("ru-RU") + " ₽ в год"
-                          : handleValue >= elem.max && handleValue < elem.ultra
-                          ? Number((handleValue * elem.income_max) / 100).toLocaleString("ru-RU") + " ₽ в год"
-                          : Number((handleValue * elem.income_ultra) / 100).toLocaleString("ru-RU") + " ₽ в год"}
-                      </p>
-                    </div>
-                  </div>
+                <div className={elem.key} onClick={(event) => findProjectOnTap(event, elem.key)}>
                   <div
-                    className={
-                      openFullProjectDescription
-                        ? "small_mobail_project_card__arrow_to_down small_mobail_project_card__arrow_to_up"
-                        : "small_mobail_project_card__arrow_to_down"
-                    }
-                  ></div>
+                    className="small_mobail_project_card__color_visible"
+                    style={{ backgroundImage: `url('${elem.background}')` }}
+                    onClick={() => setOpenFullProjectDescription(!openFullProjectDescription)}
+                  >
+                    <div className="small_mobail_project_card__logo" style={{ backgroundImage: `url('${elem.logo}')` }}></div>
+                    <div className="small_mobail_project_card__income">
+                      <p className="small_mobail_project_card_income_percent_numbers">
+                        {handleValue < elem.mid
+                          ? elem.income_min + "% годовых"
+                          : handleValue >= elem.mid && handleValue < elem.max
+                          ? elem.income_mid + "% годовых"
+                          : handleValue >= elem.max && handleValue < elem.ultra
+                          ? elem.income_max + "% годовых"
+                          : elem.income_ultra + "% годовых"}
+                      </p>
+                      <div className="small_mobail_project_card__income_money">
+                        <p className="small_mobail_project_card_income_money_numbers">
+                          {handleValue < elem.mid
+                            ? Number((handleValue * elem.income_min) / 100).toLocaleString("ru-RU") + " ₽ в год"
+                            : handleValue >= elem.mid && handleValue < elem.max
+                            ? Number((handleValue * elem.income_mid) / 100).toLocaleString("ru-RU") + " ₽ в год"
+                            : handleValue >= elem.max && handleValue < elem.ultra
+                            ? Number((handleValue * elem.income_max) / 100).toLocaleString("ru-RU") + " ₽ в год"
+                            : Number((handleValue * elem.income_ultra) / 100).toLocaleString("ru-RU") + " ₽ в год"}
+                        </p>
+                      </div>
+                    </div>
+                    <div
+                      className={
+                        openFullProjectDescription && useProjectKey === elem.key
+                          ? "small_mobail_project_card__arrow_to_down small_mobail_project_card__arrow_to_up"
+                          : "small_mobail_project_card__arrow_to_down"
+                      }
+                    ></div>
+                  </div>
                 </div>
                 {/* 4. Раскрывающийся блок */}
                 <div className="small_mobail_after_click__project_block">
