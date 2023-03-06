7c487f2936cc22be8e997a48fced2ddb2342d954
NadyaKorvin <nadya.korvin@gmail.com>
2023-03-06 22:30:56 +0700

    new numbers first screen

--- src/components/firstScreen/FirstScreen.js
+++ src/components/firstScreen/FirstScreen.js
@@ -13,8 +13,8 @@ export default function firstScreen() {
         <div className="firstscreen__content_rihgt_text">
           <div className="firstscreen__content_rihgt_text_up">
             <p>
-              Frontiers — инвестиционное агентство, работаем c 2019 года. Только в 2021 году мы помогли частным инвесторам заработать 50 млн рублей на
-              вложениях в российский бизнес.
+              Frontiers — инвестиционное агентство, работаем c 2019 года. Только в 2022 году мы помогли частным инвесторам заработать 252 млн рублей
+              на вложениях в российский бизнес.
             </p>
           </div>
           <div>
